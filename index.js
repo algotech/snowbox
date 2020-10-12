e92b34a4051524784ee6e62d10e53061dcea9a00
Login Anca <login@algotech.solutions>
2020-10-12 10:29:35 +0300

    Add root index.js.

---
+++ index.js
@@ -0,0 +1,7 @@
+export { default as api } from './src/api';
+export { default as Provider } from './src/provider';
+export { entity } from './src/entity';
+export { upsert, remove, find, fetch } from './src/actions';
+export { selectOne, selectMany } from './src/selectors';
+export { snowboxReducer } from './src/reducers';
+export { snowboxMiddleware } from './src/middleware';
--- src/index.js
+++
@@ -1,7 +0,0 @@
-export { default as api } from './api';
-export { default as Provider } from './provider';
-export { entity } from './entity';
-export { upsert, remove, find, fetch } from './actions';
-export { selectOne, selectMany } from './selectors';
-export { snowboxReducer } from './reducers';
-export { snowboxMiddleware } from './middleware';
