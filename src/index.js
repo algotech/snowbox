6973680d148d7611297eb30569a416605b4b4fe1
Login Anca <login@algotech.solutions>
2020-10-12 10:15:54 +0300

    Fix the exports.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-export api from './api';
-export Provider from './provider';
+export { default as api } from './api';
+export { default as Provider } from './provider';
 export { entity } from './entity';
 export { upsert, remove, find, fetch } from './actions';
 export { selectOne, selectMany } from './selectors';
