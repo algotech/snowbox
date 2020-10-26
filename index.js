8b534e4f7f747abdab93588b8b79270e34eecead
Login Anca <login@algotech.solutions>
2020-10-26 21:29:50 +0200

    Add export to index.js.

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ export { upsert, remove, find, fetch } from './src/actions';
 export { selectOne, selectMany } from './src/selectors';
 export { snowboxReducer } from './src/reducers';
 export { snowboxMiddleware } from './src/middleware';
+export { default as withForm } from './src/form';
