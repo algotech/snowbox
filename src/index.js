43f40149ec9f5927267f85e924bbc54ba96df84e
Login Anca <login@algotech.solutions>
2021-03-15 08:03:04 +0200

    Export clearAll.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 export { default as api } from './api';
 export { default as Provider } from './provider';
 export { entity } from './entity';
-export { upsert, remove, find, fetch } from './actions';
+export { upsert, remove, find, fetch, clearAll } from './actions';
 export { selectOne, selectMany } from './selectors';
 export { snowboxReducer } from './reducers';
 export { snowboxMiddleware } from './middleware';
 export { default as withForm } from './withForm';
-export { contentTypes } from './constants';
+export { contentTypes, actions } from './constants';
