da3033098525a1e2ebf0b463c1d3c9e7956a503f
Login Anca <login@algotech.solutions>
2021-09-29 13:06:26 +0300

    Remove examples folder.

--- examples/todos-with-list-hook/src/provider.js
+++
@@ -1,9 +0,0 @@
-import { api, provider } from 'snowbox';
-
-export const appApi = api({
-	baseUrl: 'http://localhost:4444',
-});
-
-export const appProvider = provider(appApi);
-
-export default appProvider;
--- examples/todos/src/components/Todos/TodosRedux.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import { fetch } from 'snowbox';
-
-import todo from '../../entities/todo';
-
-import Todos from './Todos';
-
-export default connect(null, { fetchTodos: fetch([todo]) })(Todos);
